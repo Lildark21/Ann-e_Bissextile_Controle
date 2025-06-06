4b9dae2af3465a6b7196dd1b633eab98f0790416
Lildark21 <lilianorrit@gmail.com>
2025-06-06 16:17:47 +0200

    test rouge

--- Année_Bissextile_Controle/LeapYear.cs
+++ Année_Bissextile_Controle/LeapYear.cs
@@ -12,6 +12,7 @@ namespace Année_Bissextile_Controle
         {
             if (year % 400 == 0) return true;
             if (year % 100 == 0) return false;
+            if (year % 4 == 0) return true;
             return false;
         }
     }
