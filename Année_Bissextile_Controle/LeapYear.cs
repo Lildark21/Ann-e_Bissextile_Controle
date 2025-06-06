b26ef869058485bcf88d863090bf2b8a09052b02
Lildark21 <lilianorrit@gmail.com>
2025-06-06 15:52:00 +0200

    Teste au rouge

---
+++ Année_Bissextile_Controle/LeapYear.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Année_Bissextile_Controle
+{
+    public class LeapYear
+    {
+        public static bool IsLeap(int year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,11 +1,14 @@
+using Xunit;
+using Année_Bissextile_Controle;
+
 namespace TestProject1
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void YearDivisibleBy400_IsLeap()
         {
-
+            Assert.True(LeapYear.IsLeap(2000));
         }
     }
 }
